d35ea05a7756c954ebcb97d934c6ddb793cf5781
efreet <efreet111@gmail.com>
2025-04-06 12:02:20 -0500

    Creacion de listado de guitarras desde un archivo plano

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import { db } from './data/db';
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
 
-function App() {
+function App()
+{
+    const [data, setData] = useState(db)
 
-
-  return (
-    <>
-
-      <h1>Guitar La React</h1>
+    return (
+        <>
+    <Header />
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+                    {/* {data.map((guitar) => (
+                        <Guitar key={guitar.id} guitar={guitar}/>
+                    ))} */}
+                    {data.map((guitar) => (
+                        <Guitar 
+                        key={guitar.id}
+                        guitar = {guitar}
+                        /> 
+                    ))}
+        </div>
+    </main>
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA – Todos los derechos Reservados</p>
+        </div>
+    </footer>
     </>
   )
 }
 
-export default App
+export default App;
+
